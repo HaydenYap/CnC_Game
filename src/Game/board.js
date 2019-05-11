d5bb505d4ac4dd2504286cd77fbc399fdfa127fc
Patrick Zhang <patrickzhangyf@hotmail.com>
2019-05-11 19:40:29 -0400

    updated board.js

---
+++ src/Game/board.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './game.scss';
+
+class Board extends React.Component{
+    componentDidMount () {
+      const canvas = this.refs.gameBoard
+      console.log("hmm", canvas)
+      const ctx = canvas.getContext('2d');
+      ctx.fillRect(0, 0, canvas.width, canvas.height)
+    }
+
+    render(){
+        return(
+            <div>
+              <canvas ref="gameBoard" width={640} height={640} />
+            </div>
+        )
+    }
+}
+
+export default Board;
