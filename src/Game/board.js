acc15d5fb2780e386d242ca2a5f3c7376eb7e4a8
HaydenYap <haydenphann@gmail.com>
2019-05-11 19:46:26 -0400

    grid added

--- src/Game/board.js
+++ src/Game/board.js
@@ -1,18 +1,43 @@
 import React from 'react';
 import './game.scss';
 
+function drawGrid(canvasContext, canvas){
+  canvasContext.strokeStyle = 'grey';
+  var width = canvas.width;
+  var horizontalDistance = width / 30;
+  var height = canvas.height;
+  var verticalDistance = height / 30;
+  console.log(width)
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+  for (var vertical = horizontalDistance; vertical < width; vertical += horizontalDistance){
+    canvasContext.beginPath();
+    canvasContext.moveTo(vertical, 0);
+    canvasContext.lineTo(vertical, height);
+    canvasContext.stroke();
+  }
+  
+  for (var horizontal = verticalDistance; horizontal < height; horizontal += verticalDistance){
+    canvasContext.beginPath();
+    canvasContext.moveTo(0, horizontal);
+      canvasContext.lineTo(width ,horizontal);
+      canvasContext.stroke();
+  }
+}
+
 class Board extends React.Component{
     componentDidMount () {
       const canvas = this.refs.gameBoard
       console.log("hmm", canvas)
       const ctx = canvas.getContext('2d');
       ctx.fillRect(0, 0, canvas.width, canvas.height)
+      drawGrid(ctx, canvas);
+
     }
 
     render(){
         return(
             <div>
-              <canvas ref="gameBoard" width={640} height={640} />
+              <canvas ref="gameBoard" width={780} height={780} />
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -10,49 +10,49 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-window.requestAnimationFrame(draw2);
+// window.requestAnimationFrame(draw2);
 
-window.onresize = draw2;
+// window.onresize = draw2;
 
-function draw2() {
+// function draw2() {
 
-    if (canvas.width !== window.innerWidth || canvas.height !== window.innerHeight) {
-        canvas.width = window.innerWidth - (window.innerWidth % 20);
-        canvas.height = window.innerHeight - (window.innerHeight % 20);
-    } else {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
-    ctx.translate(0.5,0.5)
-    window.requestAnimationFrame(draw)
-    window.requestAnimationFrame(drawGrid)
-}
+//     if (canvas.width !== window.innerWidth || canvas.height !== window.innerHeight) {
+//         canvas.width = window.innerWidth - (window.innerWidth % 20);
+//         canvas.height = window.innerHeight - (window.innerHeight % 20);
+//     } else {
+//         ctx.clearRect(0, 0, canvas.width, canvas.height);
+//     }
+//     ctx.translate(0.5,0.5)
+//     window.requestAnimationFrame(draw)
+//     window.requestAnimationFrame(drawGrid)
+// }
 
-function draw(){
-    ctx.fillStyle = 'green';
-    ctx.fillRect(0, 0, 20, 20);
-}
+// function draw(){
+//     ctx.fillStyle = 'green';
+//     ctx.fillRect(0, 0, 20, 20);
+// }
 
-function drawGrid(){
-    ctx.strokeStyle = 'grey';
-    var width = canvas.width;
-    var horizontalDistance = width / 30;
-    var height = canvas.height;
-    var verticalDistance = height / 30;
-    console.log(width)
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
-    // for (var vertical = horizontalDistance; vertical < width; vertical += horizontalDistance){
-    //     ctx.beginPath();
-    //     ctx.moveTo(vertical, 0);
-    //     ctx.lineTo(vertical, height);
-    //     ctx.stroke();
-    // }
-    //
-    // for (var horizontal = verticalDistance; horizontal < height; horizontal += verticalDistance){
-    //     ctx.beginPath();
-    //     ctx.moveTo(0, horizontal);
-    //     ctx.lineTo(width ,horizontal);
-    //     ctx.stroke();
-    // }
-}
+// function drawGrid(){
+//     ctx.strokeStyle = 'grey';
+//     var width = canvas.width;
+//     var horizontalDistance = width / 30;
+//     var height = canvas.height;
+//     var verticalDistance = height / 30;
+//     console.log(width)
+//     ctx.fillRect(0, 0, canvas.width, canvas.height)
+//     // for (var vertical = horizontalDistance; vertical < width; vertical += horizontalDistance){
+//     //     ctx.beginPath();
+//     //     ctx.moveTo(vertical, 0);
+//     //     ctx.lineTo(vertical, height);
+//     //     ctx.stroke();
+//     // }
+//     //
+//     // for (var horizontal = verticalDistance; horizontal < height; horizontal += verticalDistance){
+//     //     ctx.beginPath();
+//     //     ctx.moveTo(0, horizontal);
+//     //     ctx.lineTo(width ,horizontal);
+//     //     ctx.stroke();
+//     // }
+// }
 
 serviceWorker.unregister();
