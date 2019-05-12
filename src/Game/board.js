0b8084ad2879aededde1171ae69c0605838e3d74
yanfii <patrickzhangyf@hotmail.com>
2019-05-11 20:13:49 -0400

    added in states

--- src/Game/board.js
+++ src/Game/board.js
@@ -1,42 +1,60 @@
 import React from 'react';
 import './board.scss';
 
-function drawGrid(canvasContext, canvas){
-  canvasContext.strokeStyle = 'grey';
-  var width = canvas.width;
-  var horizontalDistance = width / 30;
-  var height = canvas.height;
-  var verticalDistance = height / 30;
-  console.log(width)
-  canvasContext.fillRect(0, 0, canvas.width, canvas.height)
-  for (var vertical = horizontalDistance; vertical < width; vertical += horizontalDistance){
-    canvasContext.beginPath();
-    canvasContext.moveTo(vertical, 0);
-    canvasContext.lineTo(vertical, height);
-    canvasContext.stroke();
-  }
-  
-  for (var horizontal = verticalDistance; horizontal < height; horizontal += verticalDistance){
-    canvasContext.beginPath();
-    canvasContext.moveTo(0, horizontal);
-      canvasContext.lineTo(width ,horizontal);
-      canvasContext.stroke();
-  }
-}
+
+// function draw(){
+//     ctx.fillStyle = 'green';
+//     ctx.fillRect(0, 0, 20, 20);
+// }
 
 class Board extends React.Component{
+    constructor (props) {
+      super(props)
+      this.state = {}
+    }
+
+    drawGrid() {
+      const {canvas, ctx} = this.state
+      
+      ctx.strokeStyle = 'grey';
+      var width = canvas.width;
+      var horizontalDistance = width / 30;
+      var height = canvas.height;
+      var verticalDistance = height / 30;
+      console.log(width)
+      ctx.fillRect(0, 0, canvas.width, canvas.height)
+      for (var vertical = horizontalDistance; vertical < width; vertical += horizontalDistance){
+        ctx.beginPath();
+        ctx.moveTo(vertical, 0);
+        ctx.lineTo(vertical, height);
+        ctx.stroke();
+      }
+
+      for (var horizontal = verticalDistance; horizontal < height; horizontal += verticalDistance){
+        ctx.beginPath();
+        ctx.moveTo(0, horizontal);
+          ctx.lineTo(width ,horizontal);
+          ctx.stroke();
+      }
+    }
+
+    drawBackground () {
+      const {canvas, ctx} = this.state
+      ctx.fillRect(0, 0, canvas.width, canvas.height)
+    }
+
     componentDidMount () {
       const canvas = this.refs.gameBoard
-      const ctx = canvas.getContext('2d');
-      ctx.fillRect(0, 0, canvas.width, canvas.height)
-      drawGrid(ctx, canvas);
-
+      this.state.canvas = canvas
+      this.state.ctx = canvas.getContext('2d')
+      this.drawBackground();
+      this.drawGrid();
     }
 
     render(){
         return(
-            <div id="canvasContainer" className="p-5">
-              <canvas id="gameCanvas" ref="gameBoard" width={780} height={780} />
+            <div>
+              <canvas ref="gameBoard" width={780} height={780} />
             </div>
         )
     }
